d36700ae2784c88edd8e4241b0d007afae04f285
Paresh Kumar Rout Kumar <101355021+Routparesh@users.noreply.github.com>
2022-09-24 08:47:41 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+hours = float(input("Enter Hours: "))
+rate =  float(input("Enter Rate: "))
+overtime = hours-40
+if hours > 40:
+  total_overtime_pay =round(40*rate+overtime*rate* 1.5, 2)
+  print(f"pay: {total_overtime_pay}" )
+else:
+  pay = round(hours * rate, 2)
+  print(f"pay: {pay}")
